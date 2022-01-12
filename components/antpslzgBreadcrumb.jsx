99fa512d3bf2f05603d59ee4f7256485b9ea7840
IvanNess <chai@bk.ru>
2022-01-12 10:06:40 +0100

    all pages. resolving errors 4

--- components/antpslzgBreadcrumb.jsx
+++ components/antpslzgBreadcrumb.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '../styles/AntpslzgBreadcrumb.module.css'
 import { Breadcrumb } from 'antd';
+import Link from 'next/link';
 
 const AntpslzgBreadcrumb = ({pathArr}) => {
     return (
@@ -12,7 +13,7 @@ const AntpslzgBreadcrumb = ({pathArr}) => {
                     </Breadcrumb.Item>
                     {pathArr.map(path=>(
                         <Breadcrumb.Item>
-                            {path.href? <a href={path.href}>{path.title}</a>: path.title}
+                            {path.href? <Link href={path.href}><a>{path.title}</a></Link>: path.title}
                         </Breadcrumb.Item>
                     ))}
                 </Breadcrumb>
